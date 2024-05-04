8e3cabe2026efb660df1eda8f05420080e5c8af9
vivekraj9 <vivek.raj@mitaoe.ac.in>
2024-05-04 14:20:36 +0530

    Initial

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as HomeLayout } from "./HomeLayout";
+export { default as Landing } from "./Landing";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
+export { default as Logout } from "./Logout";
+export {default as Home} from "./Home";
+export {default as html5QrcodePlugin} from "./html5QrcodePlugin";
+export {default as product} from "./product";
